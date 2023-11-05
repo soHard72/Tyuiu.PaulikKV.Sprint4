7fcf055d2dfd2ab5b306759bd4d2f8aa3867d075
paulikkirill@gmail.com <paulikkirill@gmail.com>
2023-11-05 20:16:03 +0500

    Task0

--- Tyuiu.PaulikKV.Sprint4.Task0.V9.Lib/DataService.cs
+++ Tyuiu.PaulikKV.Sprint4.Task0.V9.Lib/DataService.cs
@@ -12,6 +12,7 @@ namespace Tyuiu.PaulikKV.Sprint4.Task0.V9.Lib
         public int GetSumEvenArrEl(int[] array)
         {
             int sum = 0;
+
             for (int i = 0; i <= array.Length - 1; i++)
             {
                 if (array[i] % 2 != 0)
--- Tyuiu.PaulikKV.Sprint4.Task0.V9.Test/DataServiceTest.cs
+++ Tyuiu.PaulikKV.Sprint4.Task0.V9.Test/DataServiceTest.cs
@@ -15,6 +15,7 @@ namespace Tyuiu.PaulikKV.Sprint4.Task0.V9.Test
             int[] numsArray = { 4, 6, 2, 8, 4, 5, 6, 9, 8, 7 };
             int res = ds.GetSumEvenArrEl(numsArray);
             int waitSum = 38;
+
             Assert.AreEqual(waitSum, res);
         }
     }
